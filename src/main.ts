2e9121bbd846d18e61d6517806f7d9bb0f7e76d7
limcpf <matathresh1740@naver.com>
2024-03-12 15:08:32 +0000

    feat: cors 추가

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,12 @@ import { AppModule } from './app.module';
 import { SwaggerModule } from '@nestjs/swagger';
 import { swaggerConfig } from './config/swagger.config';
 import { QueryFailedErrorFilter } from './common/exception/QueryFailed.error';
-import { EntityNotFoundError } from 'typeorm';
 import { EntityNotFoundErrorFilter } from './common/exception/EntityNotFound.error';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors()
 
   const document = SwaggerModule.createDocument(app, swaggerConfig);
   SwaggerModule.setup('api', app, document);
