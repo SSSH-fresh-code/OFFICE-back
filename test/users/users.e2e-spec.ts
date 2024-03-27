97e85f5744817fb13ad39796f9c66f4a48244ab2
limcpf <matathresh1740@naver.com>
2024-03-27 03:51:40 +0000

    feat: e2e 테스트 코드 작성을 위한 예광탄 코드 작성

--- test/users/users.e2e-spec.ts
+++ test/users/users.e2e-spec.ts
@@ -1,9 +1,9 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import { AppModule } from 'src/app.module';
 
-describe('AppController (e2e)', () => {
+describe('UsersController (e2e)', () => {
   let app: INestApplication;
 
   beforeEach(async () => {
@@ -15,10 +15,10 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('/ (GET)', () => {
+  it('/users (GET)', () => {
     return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+      .get('/users')
+      .expect(401)
+      .expect(res => res.body.message === "사용자 정보가 존재하지 않습니다.")
   });
 });
