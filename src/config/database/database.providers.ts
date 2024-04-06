b41343ba18095c155c092cecd9e160c4f4757a52
limcpf <matathresh1740@naver.com>
2024-04-06 13:27:29 +0000

    fix: migration 수정

--- src/config/database/database.providers.ts
+++ src/config/database/database.providers.ts
@@ -1,10 +1,6 @@
 import { registerAs } from "@nestjs/config";
 import { readFileSync } from "fs";
 import { DataSource, DataSourceOptions } from "typeorm";
-import { config } from 'dotenv';
-import { resolve } from 'path';
-
-config({ path: resolve(__dirname, '../../../.env') });
 
 function getTypeOrmConfig(): DataSourceOptions {
   const isProduction = process.env.NEST_MODE === "production";
