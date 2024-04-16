0c28603c89094d2d3dde8ac405140354828fb0b8
limcpf <matathresh1740@naver.com>
2024-04-16 03:35:33 +0000

    feat: 운영 사용가능한 쿼리 추가

---
+++ documents/auths.sql
@@ -0,0 +1,14 @@
+INSERT INTO "auths" ("createdAt", "updatedAt", code, description) values (now(), now(),	'S0000001',	'슈퍼권한');
+INSERT INTO "auths" ("createdAt", "updatedAt", code, description) values (now(), now(),	'LOGIN001',	'CAN_USE_OFFICE');
+INSERT INTO "auths" ("createdAt", "updatedAt", code, description) values (now(), now(),	'A0000003',	'CAN_USE_WORK');
+INSERT INTO "auths" ("createdAt", "updatedAt", code, description) values (now(), now(),	'M0000001',	'MODIFY_ANOTHER_USER');
+INSERT INTO "auths" ("createdAt", "updatedAt", code, description) values (now(), now(),	'D0000001',	'DELETE_ANOTHER_USER');
+INSERT INTO "auths" ("createdAt", "updatedAt", code, description) values (now(), now(),	'R0000001',	'READ_ANOTHER_USER');
+INSERT INTO "auths" ("createdAt", "updatedAt", code, description) values (now(), now(),	'M0000002',	'MODIFY_ALARMS');
+INSERT INTO "auths" ("createdAt", "updatedAt", code, description) values (now(), now(),	'D0000002',	'DELETE_ALARMS');
+INSERT INTO "auths" ("createdAt", "updatedAt", code, description) values (now(), now(),	'R0000002',	'READ_ALARMS');
+INSERT INTO "auths" ("createdAt", "updatedAt", code, description) values (now(), now(),	'P0000002',	'POST_ALARMS');
+INSERT INTO "auths" ("createdAt", "updatedAt", code, description) values (now(), now(),	'M0000003',	'MODIFY_ANOTHER_WORK');
+INSERT INTO "auths" ("createdAt", "updatedAt", code, description) values (now(), now(),	'D0000003',	'DELETE_ANOTHER_WORK');
+INSERT INTO "auths" ("createdAt", "updatedAt", code, description) values (now(), now(),	'R0000003',	'READ_ANOTHER_WORK');
+INSERT INTO "auths" ("createdAt", "updatedAt", code, description) values (now(), now(),	'AR000001',	'ADMIN_ALARMS');
